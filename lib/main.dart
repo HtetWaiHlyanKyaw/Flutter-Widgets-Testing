71b870427e651e9c41540c979443301b9dcbcdcd
Jaz <htetwaihlyankyaw27@gmail.com>
2024-06-21 17:18:21 +0630

    Stacks and GestureDetector

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,37 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void userTapped() {
+    print("User Tapped");
+  }
+
+  // List names = ["Htet Wai", "Thiri"];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          // backgroundColor: Colors.deepPurple[200],
-          // appBar: AppBar(
-          //   title: Text("My App Bar",
-          //       style: TextStyle(color: Colors.white, fontSize: 18)),
-          //   centerTitle: true,
-          //   backgroundColor: Colors.deepPurple[500],
-          //   leading: Icon(
-          //     Icons.menu,
-          //     color: Colors.white,
-          //   ),
-          //   actions: [
-          //     IconButton(
-          //         onPressed: () {},
-          //         icon: Icon(
-          //           Icons.logout,
-          //           color: Colors.white,
-          //         ))
-          //   ],
-          // ),
-          // body: Center(
-          //   child: Container(
-          //     height: 300,
-          //     width: 300,
-          //     decoration: BoxDecoration(
-          //       color: Colors.deepPurple[500],
-          //       borderRadius: BorderRadius.circular(25),
-          //     ),
-          //     padding: EdgeInsets.all(25),
-          //     child: Icon(
-          //       Icons.favorite,
-          //       color: Colors.white,
-          //       size: 64,
-          //     ),
-          //     // child: Text(
-          //     //   "Htet Wai",
-          //     //   style: TextStyle(
-          //     //     color: Colors.white,
-          //     //     fontSize: 28,
-          //     //     fontWeight: FontWeight.bold,
-          //     //   ),
-          //     // ),
-          //   ),
-          // ),
-          body: ListView(
-        // mainAxisAlignment: MainAxisAlignment.start,
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        scrollDirection: Axis.horizontal,
-        children: [
-          Container(
-            width: 350,
-            color: Colors.deepPurple,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: GestureDetector(
+              onTap: () {
+                userTapped();
+              },
+              child: Container(
+                height: 200,
+                width: 200,
+                color: Colors.deepPurple[200],
+                child: Center(child: Text("Tap Me")),
+              ),
+            ),
           ),
-          Container(
-            width: 350,
-            color: Colors.deepPurple[400],
-          ),
-          Container(
-            width: 350,
-            color: Colors.deepPurple[200],
-          )
-        ],
-      )),
-    );
+        ));
   }
 }

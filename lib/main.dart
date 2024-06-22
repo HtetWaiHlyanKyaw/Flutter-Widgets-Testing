836267e91f53391a205003110841747afd68c7e5
Jaz <htetwaihlyankyaw27@gmail.com>
2024-06-22 13:05:49 +0630

    Counter app with stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:new_project/pages/first_page.dart';
-import 'package:new_project/pages/home_page.dart';
-import 'package:new_project/pages/settings_page.dart';
-import 'package:new_project/pages/profile_page.dart';
+import 'package:new_project/pages/counter_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,17 +10,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // List names = ["Htet Wai", "Thiri"];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: FirstPage(),
-        routes: {
-          '/firstpage': (context) => FirstPage(),
-          '/homepage': (context) => HomePage(),
-          '/settingspage': (context) => SettingsPage(),
-          '/profilepage': (context) => ProfilePage(),
-        });
+      home: CounterPage(),
+    );
   }
 }
